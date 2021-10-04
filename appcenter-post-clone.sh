f01e690f99c9ba9fcd44af183db90c6ac0d09145
tdevere <tdevere@microsoft.com>
2021-10-04 13:35:26 -0500

    fixed yarn script

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,2 +1,3 @@
 echo "tdevere"
-sh -c if [ -f yarn.lock ]; then { yarn install --network-timeout=600000 && yarn list --depth=0; } else npm install; fi
+
+yarn install --network-timeout=600000 && yarn list --depth=0
