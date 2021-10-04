a2a51f72fda986a22f112ed43773654e31999878
tdevere <tdevere@microsoft.com>
2021-10-04 13:21:10 -0500

    all proj files

---
+++ Yarn_Problems/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Yarn_Problems
+//
+//  Created by Tony DeVere on 10/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Yarn_Problems/Yarn_ProblemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yarn_ProblemsApp.swift
+//  Yarn_Problems
+//
+//  Created by Tony DeVere on 10/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Yarn_ProblemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
